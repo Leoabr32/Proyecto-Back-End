01749138c5b86c217cb7f463682ae50458cde399
leoabr32 <leoabr32@gmail.com>
2025-09-28 04:00:41 -0300

    clase02

---
+++ Clase02/nobloqueante.js
@@ -0,0 +1,11 @@
+const { log } = require("console");
+const fs = require("fs");
+
+console.log("A: inicio");
+
+fs.readFile("./text.txt", "utf8", (err, data) => {
+    console.log("C: listo el archivo (callback)", err, data);
+});
+
+console,log("B: sigo con otras cosas");
+
